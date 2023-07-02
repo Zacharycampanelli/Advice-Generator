dbffd602a18318671f298b8db881df036bfb1c92
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-07-02 18:11:46 -0400

    api call to get advice added

---
+++ src/components/Advice.jsx
@@ -0,0 +1,13 @@
+const Advice = () => {
+
+  
+
+  return (
+    <div>
+        <p>ADVICE #117</p>
+        <h2 >“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</h2>
+    </div>
+  )
+}
+
+export default Advice
--- src/components/AdviceBox.jsx
+++ src/components/AdviceBox.jsx
@@ -1,11 +1,41 @@
 import { Divider, Flex } from '@chakra-ui/react';
+import Advice from './Advice';
+import { useEffect, useState } from 'react';
 
 const AdviceBox = () => {
+    const [advice, setAdvice] = useState("");
+    const [adviceId, setAdviceId] = useState(0);
+
+  const url = 'https://api.adviceslip.com/advice';
+
+  useEffect(() => {
+
+    const fetchAdvice = async () => {
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            console.log(json);
+        } catch (err) {
+            console.log('Error: ', err);
+        }
+    };
+
+    fetchAdvice();
+}, [])
+
   return (
     <div>
-      <Flex textAlign="center" flexDirection="column" alignItems="center" justifyContent="center" bg="darkGrayBlue" borderRadius="10px" h="40vh" mt="33%">
-        <p>ADVICE #117</p>
-        <h2 >“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</h2>
+      <Flex
+        textAlign="center"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        bg="darkGrayBlue"
+        borderRadius="10px"
+        h="40vh"
+        mt="25vh"
+      >
+        <Advice />
         <Divider />
       </Flex>
     </div>
